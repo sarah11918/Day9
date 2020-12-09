3d9b7fb3bd7b88b957dda55d1b6142ab3087c4f1
Sarah Rainsberger <sarah@rainsberger.ca>
2020-12-09 21:36:16 +0000

    Fully working for n = 5, test data

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let testArray = [35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576]
-
 let results = []
+
+async function getAllData() {
+  let response = await fetch('Day9.txt')
+  let data = await response.text()
+
+  return data
+}
+
+function lines(data) {
+  return data.split("\n")
+}
+
+function parseMoves(array) {
+  let parsedMoves = array.map(move => parseInt(move))
+  return parsedMoves
+}
 
 function breakIntoSubArrays(array) {
   array.forEach(element => {
@@ -10,11 +25,12 @@ function breakIntoSubArrays(array) {
   return subArrays
 }
 
-
 function checkSums(array){
   let responses = []
-  for (let n = 0; n<array.length; n++) {
+  let problem =[]
+  for (let n = 0; n< array.length; n++){
     let checkedSums = []
+    let problems = []
     for (let i = 0; i < 5; i++) {
       for (let j = 1; j < 5; j++) {
         if (array[n][i] + array[n][i+j] > 0) {
@@ -23,64 +39,34 @@ function checkSums(array){
        
       }
     }
+ 
     responses = [...responses, [...checkedSums]]
 
   }
- //console.log("done")
  return responses
 }
 
-console.log(checkSums(breakIntoSubArrays(testArray)))
+function compare (array) {
+  for (let i = 0; i < array.length; i++) {
+    if (!array[i].includes(testArray[i + 5])) {
+      return testArray[i + 5]
+    }
+  }
+}
 
-  
-       
-//       // let answers = [
-//       //   array[5],
-//       //   ...checkedSums.slice(0,5),
-//       //   array[6],
-//       //   ...checkedSums.slice(5,10),
-//       //   array[7],
-//       //   ...checkedSums.slice(10,15),
-//       //   array[8],
-//       //   ...checkedSums.slice(15,20),
-//       //   array[9],
-//       //   ...checkedSums.slice(20,25),
-//       //   array[10],
-//       //   ...checkedSums.slice(25,30),
-//       //   array[11],
-//       //   ...checkedSums.slice(30,35),
-//       //   array[12],
-//       //   ...checkedSums.slice(35,40),
-//       //   array[13],
-//       //   ...checkedSums.slice(40,45),
-//       //   array[14],
-//       //   ...checkedSums.slice(45,50),
-//       //   array[15],
-//       //   ...checkedSums.slice(50,55),
-//       //   array[16],
-//       //   ...checkedSums.slice(55)
-//     }  
-//       // let answers = [...checkedSums]
-//       // if (answers.includes(array[i+5])) {
-//       //   continue
-//       // } else results.push(array[i+5])
 
-//   }
-//   return checkedSums
-// }
+console.log(compare(checkSums(breakIntoSubArrays(testArray))))
+// getAllData()
+// .then(lines)
+// .then(parseMoves)
+// .then(breakIntoSubArrays)
+// .then(checkSums)
+// .then(compare)
+// .then(result => console.log(result))
 
-// function returnItemOfInterest(results){
-//   let problem = []
-//   for (let i = 0; i < testArray.length; i++) {
-//     if (!results.includes(testArray[i])) {
-//       problem.push(testArray[i])
-//     }
-//   }
-//   return problem.slice(6)
-// }
-// console.log(checkSums(testArray).length)
-// console.log(returnItemOfInterest(checkSums(testArray)))
-// console.log(returnItemOfInterest(flattenResults(checkSums(testArray))))
+
+// let testArray = [35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576]
+// let results = []
 
 // async function getAllData() {
 //   let response = await fetch('Day9.txt')
@@ -98,9 +84,50 @@ console.log(checkSums(breakIntoSubArrays(testArray)))
 //   return parsedMoves
 // }
 
+// function breakIntoSubArrays(array) {
+//   array.forEach(element => {
+//   results.push([...array.slice(array.indexOf(element),array.indexOf(element) + 5)])
+//   })
+//   let subArrays = results.filter(item => item.length === 25).slice(0,-1)
+//   return subArrays
+// }
+
+// function checkSums(array){
+//   let responses = []
+//   let problem =[]
+//   for (let n = 0; n< array.length; n++){
+//     let checkedSums = []
+//     let problems = []
+//     for (let i = 0; i < 5; i++) {
+//       for (let j = 1; j < 5; j++) {
+//         if (array[n][i] + array[n][i+j] > 0) {
+//         checkedSums.push(array[n][i] + array[n][i+j])
+//         }
+       
+//       }
+//     }
+ 
+//     responses = [...responses, [...checkedSums]]
+
+//   }
+//  return responses
+// }
+
+// function compare (array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (!array[i].includes(testArray[i+5])) {
+//       return testArray[i+5]
+//     }
+//   }
+// }
+
+
+// //console.log(compare(checkSums(breakIntoSubArrays(testArray))))
 // getAllData()
 // .then(lines)
 // .then(parseMoves)
+// .then(breakIntoSubArrays)
 // .then(checkSums)
-// .then(flattenResults)
+// .then(compare)
 // .then(result => console.log(result))
+  
